9d4cf9e119273fc21071cb51a03acfb4d003cde9
LLjungar <144705053+LLjungar@users.noreply.github.com>
2023-12-20 20:47:03 +0100

    Opdateret nav og burger
    Ikke tæt på færdig, bare for push

--- burger-menu.js
+++ burger-menu.js
@@ -2,5 +2,19 @@ const burgerIcon = document.querySelector('.burger-icon');
 const menuList = document.querySelector('.menu-list');
 
 burgerIcon.addEventListener('click', function () {
+    burgerIcon.classList.toggle('show-mobile');
     menuList.classList.toggle('show-mobile');
+});
+
+// Tilføjet en ekstra lytter for at lukke burgermenuen, når der klikkes uden for den
+document.addEventListener('click', function (event) {
+    if (!event.target.closest('.menu-container')) {
+        menuList.classList.remove('show-mobile');
+        burgerIcon.classList.remove('show-mobile');
+    }
+});
+
+// Forhindre klik på burgerikonet i at lukke burgermenuen, da det allerede håndteres af den første lytter
+burgerIcon.addEventListener('click', function (event) {
+    event.stopPropagation();
 });
